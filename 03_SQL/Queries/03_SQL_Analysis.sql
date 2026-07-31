54592c63bf0e9e9d65fb362caa21b88f3297f7c0
AmanTewatia <amantewatia8398@gmail.com>
2026-07-31 15:24:27 +0530

    Update 03_SQL_Analysis.sql

--- 03_SQL/Queries/03_SQL_Analysis.sql
+++ 03_SQL/Queries/03_SQL_Analysis.sql
@@ -1,1 +1,24 @@
+/*
+==========================================================
+Project : Google Merchandise Store Customer Journey Analytics
 
+Author  : Your Name
+
+Objective:
+Analyse customer journeys, marketing campaigns,
+conversion performance, and revenue generation
+to support business decision-making.
+
+Skills Demonstrated:
+✔ Aggregate Functions
+✔ CASE Statements
+✔ GROUP BY
+✔ ORDER BY
+✔ HAVING
+✔ CTEs
+✔ Window Functions
+✔ Subqueries
+✔ Date Functions
+
+==========================================================
+*/
