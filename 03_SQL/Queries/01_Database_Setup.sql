3bc66c71de9b65fe80096320d8e1dd128848fda7
AmanTewatia <amantewatia8398@gmail.com>
2026-07-31 15:22:55 +0530

    Update 01_Database_Setup.sql

--- 03_SQL/Queries/01_Database_Setup.sql
+++ 03_SQL/Queries/01_Database_Setup.sql
@@ -1,1 +1,24 @@
+-- ===========================================
+-- Google Merchandise Store Customer Journey Analytics
+-- Database Setup
+-- ===========================================
 
+CREATE DATABASE GoogleMerchandiseStore;
+
+USE GoogleMerchandiseStore;
+
+-- Main Customer Journey Table
+
+CREATE TABLE customer_journey (
+
+    path_id INT,
+    path_step INT,
+    total_touchpoints INT,
+    campaign VARCHAR(100),
+    total_revenue DECIMAL(12,2),
+    days_to_key_event INT,
+    touchpoints VARCHAR(255),
+    conversion_rate DECIMAL(5,2),
+    key_event_date DATE
+
+);
